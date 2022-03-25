afdbd6b7f4546e810a5fa2ff483aea1569d98bd2
middleman20 <102345270+middleman20@users.noreply.github.com>
2022-03-25 11:39:04 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -41,7 +41,7 @@ async def _batch(event):
         await event.reply(r)
         return       
     if f'{event.sender_id}' in batch:
-        return await event.reply("You've already started one batch, wait for it to complete you dumbfuck owner!")
+        return await event.reply("You've already started one batch,please wait for it to complete!")
     async with Drone.conversation(event.chat_id) as conv: 
         if s != True:
             await conv.send_message("Send me the message link you want to start saving from, as a reply to this message.", buttons=Button.force_reply())
@@ -62,8 +62,8 @@ async def _batch(event):
                 return await conv.send_message("Cannot wait more longer for your response!")
             try:
                 value = int(_range.text)
-                if value > 100:
-                    return await conv.send_message("You can only get upto 100 files in a single batch.")
+                if value > 500:
+                    return await conv.send_message("You can only get upto 500 files in a single batch.")
             except ValueError:
                 return await conv.send_message("Range must be an integer!")
             s, r = await check(userbot, Bot, _link)
@@ -83,7 +83,7 @@ async def run_batch(userbot, client, sender, link, _range):
             timer = 5
         if i < 50 and i > 25:
             timer = 10
-        if i < 100 and i > 50:
+        if i < 500 and i > 50:
             timer = 15
         if not 't.me/c/' in link:
             if i < 25:
